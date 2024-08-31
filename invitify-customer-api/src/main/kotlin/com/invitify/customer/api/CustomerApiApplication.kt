50eafd2dc51d68cc2e33ed433d5a8d514ce57003
edacchi-chonmage <thrbrd1989@gmail.com>
2024-08-31 13:16:49 +0900

    Hello World

--- invitify-customer-api/src/main/kotlin/com/invitify/customer/api/CustomerApiApplication.kt
+++ invitify-customer-api/src/main/kotlin/com/invitify/customer/api/CustomerApiApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class CustomerApiApplication
-
 fun main(args: Array<String>) {
     runApplication<CustomerApiApplication>(*args)
 }
